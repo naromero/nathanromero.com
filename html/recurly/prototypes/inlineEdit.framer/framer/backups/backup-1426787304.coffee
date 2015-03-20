7261f09c7728e34c9a313e6ae2520a31c70a3d14
Nathan Romero <nathan89@gmail.com>
2015-03-20 15:27:27 -0700

    Inline Edit Prototype

---
+++ html/recurly/prototypes/inlineEdit.framer/framer/backups/backup-1426787304.coffee
@@ -0,0 +1,73 @@
+document.body.style.cursor = "auto"
+
+
+# This imports all the layers for "inlineEdit" into inlineeditLayers
+Sketch = Framer.Importer.load "imported/inlineEdit"
+
+bg = new BackgroundLayer backgroundColor: "#F7F8FA"
+
+# Default Animation Options
+Framer.Defaults.Animation =
+  curve: "cubic-bezier(0.25, 0.46, 0.45, 0.94)"
+  time: .3
+
+# Make all the imported layers available on the root
+for layerGroupName of Sketch
+  window[layerGroupName] = Sketch[layerGroupName]
+for layerGroupName of Sketch
+  Sketch[layerGroupName].originalFrame = window[layerGroupName].frame
+
+# Centered Mask
+centeredLayer = new Layer
+	width:  Sketch.interaction.width
+	height: Sketch.interaction.height
+	scale:0.5
+	backgroundColor: null
+Sketch.interaction.superLayer = centeredLayer
+centeredLayer.center()
+window.onresize = -> centeredLayer.center()
+
+offsetValue = 152
+
+form.states.add
+	"readOnly": visible: false
+backdrop.states.add
+	"readOnly": opacity: 0
+edit.states.add
+	"editMode": visible: false
+ancillarytext.states.add
+	"readOnly": y: ancillarytext.y - offsetValue
+bottomcap.states.add
+	"readOnly": y: bottomcap.y - offsetValue
+card2.states.add
+	"readOnly": y: card2.y - offsetValue
+
+form.states.switchInstant "readOnly"
+backdrop.states.switchInstant "readOnly"
+ancillarytext.states.switchInstant "readOnly"
+bottomcap.states.switchInstant "readOnly"
+card2.states.switchInstant "readOnly"
+
+
+edit.on Events.Click, ->
+	form.states.switch "default"
+	backdrop.states.switch "default"
+	ancillarytext.states.switch "default"
+	bottomcap.states.switch "default"
+	card2.states.switch "default"
+	
+	edit.states.switch "editMode"
+
+actions.on Events.Click, ->
+	form.states.switch "readOnly"
+	backdrop.states.switch "readOnly"
+	ancillarytext.states.switch "readOnly"
+	bottomcap.states.switch "readOnly"
+	card2.states.switch "readOnly"
+	
+	edit.states.switch "default"
+	
+
+
+
+
---
+++ html/recurly/prototypes/inlineEdit.framer/imported/inlineEdit/layers.json.js
@@ -0,0 +1,491 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["inlineEdit/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "id" : "1D8DB5F2-70D7-4547-AD9F-D74188D85EC8",
+    "visible" : true,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "7D3D9059-BC14-46D5-99F6-BE27CD114565",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "1A58A7D5-8745-420B-B12E-900DFB5D220A",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/DEFAULT_SITE_CURRENC-1A58A7D5-8745-420B-B12E-900DFB5D220A.png",
+              "frame" : {
+                "y" : 286,
+                "x" : 197,
+                "width" : 283,
+                "height" : 19
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 286,
+              "x" : 197,
+              "width" : 283,
+              "height" : 19
+            },
+            "name" : "DEFAULT_SITE_CURRENC"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "97946E2F-CC4E-41A1-8EA1-F9D602024A8F",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/ancillarytext-97946E2F-CC4E-41A1-8EA1-F9D602024A8F.png",
+              "frame" : {
+                "y" : 538,
+                "x" : 197,
+                "width" : 1310,
+                "height" : 26
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 538,
+              "x" : 197,
+              "width" : 1310,
+              "height" : 26
+            },
+            "name" : "ancillarytext"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "81D0ED42-93E3-4F4E-AB69-D4B1BDFE53A1",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/edit-81D0ED42-93E3-4F4E-AB69-D4B1BDFE53A1.png",
+              "frame" : {
+                "y" : 331,
+                "x" : 198,
+                "width" : 363,
+                "height" : 29
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 331,
+              "x" : 198,
+              "width" : 363,
+              "height" : 29
+            },
+            "name" : "edit"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "B1A9CDD7-6034-4975-962C-9F258A94183C",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "EC27245A-7FA1-484F-A1C7-ECCE95DCDE89",
+                "visible" : true,
+                "children" : [
+
+                ],
+                "image" : {
+                  "path" : "images\/Rectangle_30__MULTI_CURRENCY__Please_upgrade_to_th-EC27245A-7FA1-484F-A1C7-ECCE95DCDE89.png",
+                  "frame" : {
+                    "y" : 605,
+                    "x" : 162,
+                    "width" : 1634,
+                    "height" : 115
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 605,
+                  "x" : 162,
+                  "width" : 1634,
+                  "height" : 115
+                },
+                "name" : "Rectangle_30__MULTI_CURRENCY__Please_upgrade_to_th"
+              }
+            ],
+            "image" : {
+              "path" : "images\/bottomcap-B1A9CDD7-6034-4975-962C-9F258A94183C.png",
+              "frame" : {
+                "y" : 605,
+                "x" : 160,
+                "width" : 1638,
+                "height" : 160
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 605,
+              "x" : 160,
+              "width" : 1638,
+              "height" : 160
+            },
+            "name" : "bottomcap"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "29D64346-84E0-4E4D-A56C-A028A388EB6C",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "0CB72A1F-932E-4BBE-9E30-C2F28F76E03C",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : null,
+                    "id" : "11307178-33DF-4D28-8D48-D554F6303458",
+                    "visible" : true,
+                    "children" : [
+                      {
+                        "maskFrame" : null,
+                        "id" : "2C4E62A0-6F30-40A5-8D56-BC5A3EC9D911",
+                        "visible" : true,
+                        "children" : [
+                          {
+                            "maskFrame" : null,
+                            "id" : "A1407959-18C2-42E8-A10B-CABC2FFE60DA",
+                            "visible" : true,
+                            "children" : [
+                              {
+                                "maskFrame" : null,
+                                "id" : "02ECCC84-AC79-4CE4-93C4-CF7A1590FA62",
+                                "visible" : true,
+                                "children" : [
+
+                                ],
+                                "image" : {
+                                  "path" : "images\/dropdown-02ECCC84-AC79-4CE4-93C4-CF7A1590FA62.png",
+                                  "frame" : {
+                                    "y" : 361,
+                                    "x" : 632,
+                                    "width" : 16,
+                                    "height" : 24
+                                  }
+                                },
+                                "imageType" : "png",
+                                "layerFrame" : {
+                                  "y" : 361,
+                                  "x" : 632,
+                                  "width" : 16,
+                                  "height" : 24
+                                },
+                                "name" : "dropdown"
+                              }
+                            ],
+                            "image" : {
+                              "path" : "images\/dropdown_4-A1407959-18C2-42E8-A10B-CABC2FFE60DA.png",
+                              "frame" : {
+                                "y" : 361,
+                                "x" : 632,
+                                "width" : 16,
+                                "height" : 24
+                              }
+                            },
+                            "imageType" : "png",
+                            "layerFrame" : {
+                              "y" : 361,
+                              "x" : 632,
+                              "width" : 16,
+                              "height" : 24
+                            },
+                            "name" : "dropdown_4"
+                          }
+                        ],
+                        "image" : {
+                          "path" : "images\/dropdown-2C4E62A0-6F30-40A5-8D56-BC5A3EC9D911.png",
+                          "frame" : {
+                            "y" : 361,
+                            "x" : 632,
+                            "width" : 16,
+                            "height" : 24
+                          }
+                        },
+                        "imageType" : "png",
+                        "layerFrame" : {
+                          "y" : 361,
+                          "x" : 632,
+                          "width" : 16,
+                          "height" : 24
+                        },
+                        "name" : "dropdown"
+                      }
+                    ],
+                    "image" : {
+                      "path" : "images\/Rectangle_5__your@email.com-11307178-33DF-4D28-8D48-D554F6303458.png",
+                      "frame" : {
+                        "y" : 333,
+                        "x" : 198,
+                        "width" : 480,
+                        "height" : 80
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 333,
+                      "x" : 198,
+                      "width" : 480,
+                      "height" : 80
+                    },
+                    "name" : "Rectangle_5__your@email.com"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/Input_Placeholder-0CB72A1F-932E-4BBE-9E30-C2F28F76E03C.png",
+                  "frame" : {
+                    "y" : 333,
+                    "x" : 198,
+                    "width" : 480,
+                    "height" : 80
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 333,
+                  "x" : 198,
+                  "width" : 480,
+                  "height" : 80
+                },
+                "name" : "Input_Placeholder"
+              },
+              {
+                "maskFrame" : null,
+                "id" : "7D5C6B78-8DC6-4B4D-8DD3-4F574C79F454",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : null,
+                    "id" : "2105C3CD-F4F5-4514-B599-071991AB3A4B",
+                    "visible" : true,
+                    "children" : [
+
+                    ],
+                    "image" : {
+                      "path" : "images\/Rectangle_57__Primary_Action-2105C3CD-F4F5-4514-B599-071991AB3A4B.png",
+                      "frame" : {
+                        "y" : 441,
+                        "x" : 198,
+                        "width" : 313,
+                        "height" : 64
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 441,
+                      "x" : 198,
+                      "width" : 313,
+                      "height" : 64
+                    },
+                    "name" : "Rectangle_57__Primary_Action"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/actions-7D5C6B78-8DC6-4B4D-8DD3-4F574C79F454.png",
+                  "frame" : {
+                    "y" : 441,
+                    "x" : 198,
+                    "width" : 313,
+                    "height" : 64
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 441,
+                  "x" : 198,
+                  "width" : 313,
+                  "height" : 64
+                },
+                "name" : "actions"
+              }
+            ],
+            "image" : {
+              "path" : "images\/form-29D64346-84E0-4E4D-A56C-A028A388EB6C.png",
+              "frame" : {
+                "y" : 333,
+                "x" : 198,
+                "width" : 480,
+                "height" : 172
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 333,
+              "x" : 198,
+              "width" : 480,
+              "height" : 172
+            },
+            "name" : "form"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "530467A7-7C70-4AE8-9DD0-EC07CC81E9AA",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/backdrop-530467A7-7C70-4AE8-9DD0-EC07CC81E9AA.png",
+              "frame" : {
+                "y" : 249,
+                "x" : 162,
+                "width" : 1634,
+                "height" : 356
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 249,
+              "x" : 162,
+              "width" : 1634,
+              "height" : 356
+            },
+            "name" : "backdrop"
+          }
+        ],
+        "image" : {
+          "path" : "images\/card1-7D3D9059-BC14-46D5-99F6-BE27CD114565.png",
+          "frame" : {
+            "y" : 155,
+            "x" : 160,
+            "width" : 1638,
+            "height" : 610
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 155,
+          "x" : 160,
+          "width" : 1638,
+          "height" : 610
+        },
+        "name" : "card1"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "D6690B31-3910-4D9E-BB80-F9D1BF01ACC4",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "B898ADDC-9392-4131-8F02-CA2D226E96ED",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "0BDF9704-CF96-4DFA-9126-87B91733A474",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : null,
+                    "id" : "BFDB3FF0-1F25-4CEE-AFBD-5881753E5346",
+                    "visible" : true,
+                    "children" : [
+
+                    ],
+                    "image" : {
+                      "path" : "images\/tr-BFDB3FF0-1F25-4CEE-AFBD-5881753E5346.png",
+                      "frame" : {
+                        "y" : 962,
+                        "x" : 198,
+                        "width" : 1558,
+                        "height" : 30
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 962,
+                      "x" : 198,
+                      "width" : 1558,
+                      "height" : 30
+                    },
+                    "name" : "tr"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/Group-0BDF9704-CF96-4DFA-9126-87B91733A474.png",
+                  "frame" : {
+                    "y" : 962,
+                    "x" : 198,
+                    "width" : 1558,
+                    "height" : 30
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 962,
+                  "x" : 198,
+                  "width" : 1558,
+                  "height" : 30
+                },
+                "name" : "Group"
+              }
+            ],
+            "image" : {
+              "path" : "images\/Basic_Table-B898ADDC-9392-4131-8F02-CA2D226E96ED.png",
+              "frame" : {
+                "y" : 831,
+                "x" : 160,
+                "width" : 1638,
+                "height" : 198
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 831,
+              "x" : 160,
+              "width" : 1638,
+              "height" : 198
+            },
+            "name" : "Basic_Table"
+          }
+        ],
+        "image" : {
+          "path" : "images\/card2-D6690B31-3910-4D9E-BB80-F9D1BF01ACC4.png",
+          "frame" : {
+            "y" : 831,
+            "x" : 160,
+            "width" : 1638,
+            "height" : 262
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 831,
+          "x" : 160,
+          "width" : 1638,
+          "height" : 262
+        },
+        "name" : "card2"
+      }
+    ],
+    "image" : {
+      "path" : "images\/interaction-1D8DB5F2-70D7-4547-AD9F-D74188D85EC8.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 1956,
+        "height" : 1282
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 1956,
+      "height" : 1282
+    },
+    "name" : "interaction"
+  }
+]
